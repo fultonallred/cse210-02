229b19437956b646f5d4f298f807ed7420d7a49e
uriassm <uriassmira@gmail.com>
2022-05-05 17:18:17 -0700

    Added "hilo" folder and main python program. Added "game" folder under "hilo" folder. Created card and director python files. Created "Card" class and "Director" class to use in main python file.

---
+++ hilo/game/director.py
@@ -0,0 +1,65 @@
+from game.card import Card
+
+class Director:
+    '''   
+    Attributes:
+        card (List[Card]): A list of Card instances.
+        is_playing (boolean): Whether or not the game is being played.
+        score (int): The score for one round of play.
+        total_score (int): The score for the entire game.'''
+
+    def __init__(self):
+        # We only need one instance per turn so self.card can equal the Card class directly. 
+        self.card = Card()
+        #A place to hold the last turn's value.
+        self.last_value = 0
+        #Boolean is true to start the game.
+        self.is_playing = True
+        #Game points start at 300.
+        self.score = 300
+        #A place to keep the user input for higher or lower. 
+        self.higher_lower = ''
+
+    def start_game(self):
+        while self.is_playing:
+            card = self.card
+            card.draw_card()
+            self.last_value = card.value
+            print()
+            print(f'The card is {card.value}')
+
+            self.get_higher_lower()
+            self.do_updates()
+            self.do_outputs()
+            
+    def get_higher_lower(self):
+        higher_lower = input('Higher or lower? [h/l]: ')
+        self.higher_lower = higher_lower
+    
+    def do_updates(self):
+        card = self.card
+        card.draw_card()
+        if card.value > self.last_value and self.higher_lower == 'h':
+            self.score += 100
+        elif card.value > self.last_value and self.higher_lower == 'l':
+            self.score -= 75
+        elif card.value < self.last_value and self.higher_lower == 'h':
+            self.score -= 75
+        elif card.value < self.last_value and self.higher_lower == 'l':
+            self.score += 100 
+
+    def do_outputs(self):
+        card = self.card
+        print(f'The next card was {card.value}.')
+        print(f'Your score is {self.score}')
+
+        if self.score > 0:
+            play_again = input('Play again? [y/n]: ')
+            self.is_playing = (play_again == 'y')
+
+        else:
+            self.is_playing = False
+
+
+
+        
